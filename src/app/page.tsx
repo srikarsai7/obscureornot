ef1d64a0c42f1eb8f8c2d5a888a1ed710fe7263b
Srikar Sai Amirneni <75658246+srikarsai7@users.noreply.github.com>
2025-04-01 19:17:16 -0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="center"> Welcome to Obscure or Not</h1>
+        <p>If you believe the shown fact is false choose obscure if you believe it's true choose Obscure.</p>
+        <div className="flex flex-col gap-8">
+          <a href="/game" className="btn">
+            Start Game
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}
